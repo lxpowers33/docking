6f976c8013a7f7ec3d081f861c50b6681544aa31
Alex Powers <lxpowers@sh-ln01.stanford.edu>
2019-07-24 15:25:57 -0700

    changes to setup

--- setup.py
+++ setup.py
@@ -1,9 +1,10 @@
 #!/usr/bin/env python
-import setuptools
+from setuptools import setup, find_packages
 
-setuptools.setup(
+setup(
     name="docking",
     version="0.0.1",
+    packages=['docking'],
     author="Alex Powers",
     author_email="lxpowers@stanford.edu",
     description="Package for docking tasks",
